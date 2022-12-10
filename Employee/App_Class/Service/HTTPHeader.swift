22fb75bc3f994f1122fa1bee6a1e39edff439d97
stlhorizon <anand@stl-horizon.com>
2022-12-10 12:57:54 +0530

    Updated Web Service Calling
    - Added Web service calling code

---
+++ Employee/App_Class/Service/HTTPHeader.swift
@@ -0,0 +1,30 @@
+//
+//  HTTPHeader.swift
+import Foundation
+
+enum HTTPHeaderField: String {
+    case contentType = "Content-Type"
+    case authorization = "Authorization"
+    case accept = "Accept"
+}
+
+struct HTTPHeader {
+    var field: String
+    var value: String
+    
+    init(field: HTTPHeaderField, value: String){
+        self.field = field.rawValue
+        self.value = value
+    }
+    
+    init(field: String, value: String){
+        self.field = field
+        self.value = value
+    }
+}
+extension URLRequest {
+    mutating func add(header: HTTPHeader){
+        self.addValue(header.value, forHTTPHeaderField: header.field)
+    }
+}
+
